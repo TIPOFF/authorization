125e266a8dfdda1c8f23fa155f287caf110dc8d6
auxiliawebsitedesign <preston@auxiliawebsitedesign.com>
2021-02-13 17:51:47 -0500

    Update composer.json and AuthorizationServiceProvider.php

--- src/AuthorizationServiceProvider.php
+++ src/AuthorizationServiceProvider.php
@@ -4,27 +4,19 @@ declare(strict_types=1);
 
 namespace Tipoff\Authorization;
 
-use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Tipoff\Authorization\Models\Authorization;
+use Tipoff\Authorization\Policies\AuthorizationPolicy;
 use Tipoff\Support\TipoffPackage;
 use Tipoff\Support\TipoffServiceProvider;
 
-class AuthorizationServiceProvider extends PackageServiceProvider
+class AuthorizationServiceProvider extends TipoffServiceProvider
 {
-    public function boot()
+    public function configureTipoffPackage(TipoffPackage $package): void
     {
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-        parent::boot();
-    }
-
-    public function configurePackage(Package $package): void
-    {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
+            ->hasPolicies([
+                Authorization::class => AuthorizationPolicy::class,
+            ])
             ->name('authorization')
             ->hasConfigFile();
     }
