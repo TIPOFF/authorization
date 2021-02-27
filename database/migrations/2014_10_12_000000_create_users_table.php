02feef5615abf6db2ba49e97b8204883c5255032
drewroberts <github@drewroberts.com>
2021-02-27 08:19:19 -0500

    Include default laravel migrations #20

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,14 +11,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('name_last');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->text('bio')->nullable(); // Will be written in Markdown. The user profile image will come from Gravatar account for the email address.
-            $table->text('title')->nullable();
-            $table->softDeletes();
             $table->timestamps();
         });
     }
---
+++ database/migrations/2014_10_12_000001_modify_users_table.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('name_last')->after('creator_id');
+            $table->text('bio')->nullable()->after('password'); // Will be written in Markdown. The user profile image will come from Gravatar account for the email address.
+            $table->text('title')->nullable()->after('bio');
+            $table->softDeletes()->after('title');
+        });
+    }
+}
