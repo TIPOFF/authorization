349a50f07a9a567f0513da0aed927d74845a9ce8
drewroberts <github@drewroberts.com>
2021-02-27 07:51:15 -0500

    Remove nonexistent role registrar

--- database/migrations/2021_01_01_200000_add_roles.php
+++ database/migrations/2021_01_01_200000_add_roles.php
@@ -4,15 +4,12 @@ declare(strict_types=1);
 
 use Illuminate\Database\Migrations\Migration;
 use Spatie\Permission\Contracts\Role;
-use Spatie\Permission\RoleRegistrar;
 
 class AddRoles extends Migration
 {
     public function up()
     {
         if (app()->has(Role::class)) {
-            app(RoleRegistrar::class)->forgetCachedPermissions();
-
             foreach ([
                 'Admin',
                 'Owner',
