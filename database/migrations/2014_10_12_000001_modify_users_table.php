3811a57ff579c572b7ba9986fdafda280e3fedfe
Patrick Breen <patrick.breen@quartzy.com>
2021-03-01 19:28:00 -0500

    Limit default to testing connection

--- database/migrations/2014_10_12_000001_modify_users_table.php
+++ database/migrations/2014_10_12_000001_modify_users_table.php
@@ -11,7 +11,12 @@ class ModifyUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('name_last')->default('')->after('creator_id');
+            $column = $table->string('name_last')->after('creator_id');
+            // Workaround for Sqlite / ALTER
+            // - https://stackoverflow.com/questions/20822159/laravel-migration-with-sqlite-cannot-add-a-not-null-column-with-default-value-n
+            if (config("database.default") === "testing") {
+                $column->default('');
+            }
             $table->text('bio')->nullable()->after('password'); // Will be written in Markdown. The user profile image will come from Gravatar account for the email address.
             $table->text('title')->nullable()->after('bio');
             $table->softDeletes()->after('title');
