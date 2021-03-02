321ef50a89ab8e5467a80243ad519754216a206f
devjk1 <devjk1@users.noreply.github.com>
2021-03-02 20:05:13 +0000

    Fix styling

--- src/Permissions/BasePermissionsMigration.php
+++ src/Permissions/BasePermissionsMigration.php
@@ -6,8 +6,8 @@ namespace Tipoff\Authorization\Permissions;
 
 use Illuminate\Database\Migrations\Migration;
 use Spatie\Permission\Contracts\Permission;
-use Spatie\Permission\PermissionRegistrar;
 use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
 
 class BasePermissionsMigration extends Migration
 {
