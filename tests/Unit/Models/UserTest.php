adc75ac3361a26f4099ce30117791f9758cd6280
Patrick Breen <patrick.breen@quartzy.com>
2021-03-02 12:10:30 -0500

    Fix migration for sqlite again.  Add tests to verify migration

--- tests/Unit/Models/UserTest.php
+++ tests/Unit/Models/UserTest.php
@@ -16,4 +16,35 @@ class UserTest extends TestCase
         $user = User::factory()->create();
         $this->assertNotNull($user);
     }
+
+    /** @test */
+    public function custom_fields()
+    {
+        $user = User::factory()->create([
+            'bio' => 'bio field',
+            'title' => 'title field',
+            'provider_name' => 'provider name field',
+            'provider_id' => 'provider id field',
+        ]);
+
+        /** @var User $result */
+        $result = User::findOrFail($user->id);
+
+        $this->assertEquals('bio field', $result->bio);
+        $this->assertEquals('title field', $result->title);
+        $this->assertEquals('provider name field', $result->provider_name);
+        $this->assertEquals('provider id field', $result->provider_id);
+    }
+
+    /** @test */
+    public function soft_delete()
+    {
+        $user = User::factory()->create();
+
+        $user->delete();
+
+        $user->refresh();
+
+        $this->assertNotNull($user->deleted_at);
+    }
 }
