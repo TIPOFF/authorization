77c819d6cb8948f70c2a1e98ef8706d5b43032af
Chris H <crishack8@gmail.com>
2021-03-02 19:31:14 -0600

    Suppress error on findOrCreate call for app container

--- src/Permissions/BasePermissionsMigration.php
+++ src/Permissions/BasePermissionsMigration.php
@@ -17,8 +17,9 @@ class BasePermissionsMigration extends Migration
             $adminRole = Role::findByName('Admin');
 
             app(PermissionRegistrar::class)->forgetCachedPermissions();
-            
+
             foreach ($permissions as $permission) {
+                /** @psalm-suppress UndefinedMethod */
                 app(Permission::class)::findOrCreate($permission, null);
                 $adminRole->givePermissionTo($permission);
             }
