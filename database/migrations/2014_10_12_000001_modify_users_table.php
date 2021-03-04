55783e51b3e3719986e615f9f8ed04d024280087
Drew Roberts <github@drewroberts.com>
2021-03-04 04:45:31 -0500

    Fix test error with sqlite workaround

--- database/migrations/2014_10_12_000001_modify_users_table.php
+++ database/migrations/2014_10_12_000001_modify_users_table.php
@@ -23,6 +23,11 @@ class ModifyUsersTable extends Migration
                 $column->default('');
             }
             $table->string('username')->unique()->after('last_name');
+            // Workaround for Sqlite / ALTER
+            // - https://stackoverflow.com/questions/20822159/laravel-migration-with-sqlite-cannot-add-a-not-null-column-with-default-value-n
+            if (config("database.default") === "testing") {
+                $column->default('');
+            }
             $table->text('bio')->nullable()->after('password'); // Will be written in Markdown. The user profile image will come from Gravatar account for the email address.
             $table->text('title')->nullable()->after('bio');
             $table->softDeletes()->after('title');
