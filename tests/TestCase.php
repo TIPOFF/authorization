ef625deab13a6cbf4e8e0720f2b296d099b52197
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-03-05 10:04:06 -0500

    add dependecy to call the NovaServiceProvider and add it to the testCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace Tipoff\Authorization\Tests;
 
+use Laravel\Nova\NovaCoreServiceProvider;
 use Spatie\Permission\PermissionServiceProvider;
 use Tipoff\Authorization\AuthorizationServiceProvider;
 use Tipoff\Support\SupportServiceProvider;
+use Tipoff\Authorization\Tests\Support\Providers\NovaPackageServiceProvider;
 use Tipoff\TestSupport\BaseTestCase;
 
 class TestCase extends BaseTestCase
@@ -17,6 +19,8 @@ class TestCase extends BaseTestCase
             SupportServiceProvider::class,
             PermissionServiceProvider::class,
             AuthorizationServiceProvider::class,
+            NovaCoreServiceProvider::class,
+            NovaPackageServiceProvider::class,
         ];
     }
 }
