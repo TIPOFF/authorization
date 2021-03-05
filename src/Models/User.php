59ee0c4aea1bdd1c74721317517b5e13b89c2abe
Patrick Breen <patrick.breen@quartzy.com>
2021-03-05 10:37:59 -0500

    Fix full name attribute

--- src/Models/User.php
+++ src/Models/User.php
@@ -56,7 +56,7 @@ class User extends BaseModel implements UserInterface
             $user->generateUsername();
         });
     }
-    
+
     public function generateUsername()
     {
         do {
@@ -128,7 +128,7 @@ class User extends BaseModel implements UserInterface
 
     public function getFullNameAttribute()
     {
-        return "{$this->name} {$this->name_last}";
+        return "{$this->first_name} {$this->last_name}";
     }
 
     public function toCustomer(array $attributes)
