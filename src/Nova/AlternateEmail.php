4b0ca70612dc8c497cc6fb5354b32804e92dc637
Drew Roberts <github@drewroberts.com>
2021-03-12 16:38:05 -0500

    Add resources nova group to alternate email

--- src/Nova/AlternateEmail.php
+++ src/Nova/AlternateEmail.php
@@ -22,6 +22,8 @@ class AlternateEmail extends BaseResource
     public static $search = [
         'id',
     ];
+    
+    public static $group = 'Resources';
 
     public function fieldsForIndex(NovaRequest $request)
     {
