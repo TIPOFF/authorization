87a4a56dc1470f463a39f33c9843537517579e06
Chris H <crishack8@gmail.com>
2021-03-21 20:38:17 -0500

    Fix EmailAddress typos

---
+++ tests/Unit/Models/EmailAddressTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tipoff\Authorization\Tests\Unit\Models;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tipoff\Authorization\Models\EmailAddress;
+use Tipoff\Authorization\Models\User;
+use Tipoff\Authorization\Tests\TestCase;
+
+class EmailAddressTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /** @test */
+    public function create()
+    {
+        $email = EmailAddress::factory()->create();
+        $this->assertNotNull($email);
+    }
+}
