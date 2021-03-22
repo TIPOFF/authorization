8c3d7a7b1eb24d98538307564b0e784a18246e6e
Hunter Montell <64396917+huntermontell@users.noreply.github.com>
2021-03-22 18:28:27 -0500

    Update user model test

--- tests/Unit/Models/UserTest.php
+++ tests/Unit/Models/UserTest.php
@@ -1,50 +1,23 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tipoff\Authorization\Tests\Unit\Models;
 
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tipoff\Authorization\Models\User;
 use Tipoff\Authorization\Tests\TestCase;
 
 class UserTest extends TestCase
 {
     use DatabaseTransactions;
+    use WithFaker;
 
     /** @test */
     public function create()
     {
-        $user = User::factory()->create();
-        $this->assertNotNull($user);
-    }
-
-    /** @test */
-    public function custom_fields()
-    {
-        $user = User::factory()->create([
-            'bio' => 'bio field',
-            'title' => 'title field',
-            'provider_name' => 'provider name field',
-            'provider_id' => 'provider id field',
-        ]);
-
-        /** @var User $result */
-        $result = User::findOrFail($user->id);
-
-        $this->assertEquals('bio field', $result->bio);
-        $this->assertEquals('title field', $result->title);
-        $this->assertEquals('provider name field', $result->provider_name);
-        $this->assertEquals('provider id field', $result->provider_id);
-    }
-
-    /** @test */
-    public function soft_delete()
-    {
-        $user = User::factory()->create();
-
-        $user->delete();
-
-        $user->refresh();
-
-        $this->assertNotNull($user->deleted_at);
+        $model = User::factory()->create();
+        $this->assertNotNull($model);
     }
 }
