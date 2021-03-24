ffe0aad5582013bed81361c90cd4a375f7acb10a
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-03-24 14:34:54 -0400

    improve nota test

---
+++ tests/Feature/Nova/EmailAddressResourceTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tipoff\Authorization\Tests\Feature\Nova;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tipoff\Authorization\Models\User;
+use Tipoff\Authorization\Tests\TestCase;
+use Tipoff\Authorization\Models\EmailAddress;
+
+class EmailAddressResourceTest extends TestCase
+{
+    use DatabaseTransactions;
+    
+    private const NOVA_ROUTE = 'nova-api/email-addresses';
+    
+    /**
+     * @dataProvider dataProviderForIndexByRole
+     * @test
+     */
+    public function index_by_role(?string $role, bool $hasAccess, bool $canIndex)
+    {
+        EmailAddress::factory()->count(4)->create();
+
+        $user = User::factory()->create();
+        if ($role) {
+            $user->assignRole($role);
+        }
+        $this->actingAs($user);
+
+        $response = $this->getJson(self::NOVA_ROUTE)
+            ->assertStatus($hasAccess ? 200 : 403);
+
+        if ($hasAccess) {
+            $this->assertCount($canIndex ? 4 : 0, $response->json('resources'));
+        }
+    }
+
+    public function dataProviderForIndexByRole()
+    {
+        return [
+            'Admin' => ['Admin', true, true],
+            'Owner' => ['Owner', true, true],
+            'Executive' => ['Executive', true, true],
+            'Staff' => ['Staff', true, true],
+            'Former Staff' => ['Former Staff', false, false],
+            'Customer' => ['Customer', false, false],
+            'No Role' => [null, false, false],
+        ];
+    }
+
+}
