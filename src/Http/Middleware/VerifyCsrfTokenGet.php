847e4c93b0d6b29dd311aae3ef6ff949fcccdf1d
Patrick Breen <patrick.breen@quartzy.com>
2021-04-15 17:16:36 -0400

    Support token in segment

--- src/Http/Middleware/VerifyCsrfTokenGet.php
+++ src/Http/Middleware/VerifyCsrfTokenGet.php
@@ -8,6 +8,15 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
 class VerifyCsrfTokenGet extends Middleware
 {
+    protected function getTokenFromRequest($request)
+    {
+        if ($token = $request->route('_token')) {
+            return (string) $token;
+        }
+
+        return parent::getTokenFromRequest($request);
+    }
+
     protected function isReading($request)
     {
         // 'GET' is not included!
